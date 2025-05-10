8ec17cde805a975fa21b171fccdcb59e8c115841
k-e-r <damajonboua@gmail.com>
2025-05-10 05:29:08 -0700

    Add timer

--- src/views/ReadingView.vue
+++ src/views/ReadingView.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
     <h2>Reading Section</h2>
+    <p><em>Time left: {{ TIME_LIMIT - timer }}s</em></p>
     <div v-if="currentQuestion">
       <p><strong>Q{{ currentIndex + 1 }}:</strong> {{ currentQuestion.text }}</p>
       <ul>
         <li v-for="(choice, i) in currentQuestion.choices" :key="i">
           <label>
-            <input type="radio" :name="'q' + currentIndex" :value="choice" v-model="userAnswer" :disabled="finished" />
+            <input type="radio" :name="'q' + currentIndex" :value="choice" v-model="userAnswer" :disabled="finished || timeout" />
             {{ choice }}
           </label>
         </li>
       </ul>
     </div>
 
-    <button @click="next" :disabled="!userAnswer || finished">Next</button>
-    <p v-if="finished">You scored {{ score }}/{{ questions.length }}</p>
+    <button @click="next" :disabled="!userAnswer && !timeout || finished">Next</button>
+    <div v-if="finished">
+      <p>You scored {{ score }}/{{ questions.length }}</p>
+      <p>Average Time: {{ averageTime.toFixed(2) }}s/question</p>
+    </div>
   </div>
 </template>
 
@@ -31,10 +35,35 @@ export default defineComponent({
     const userAnswer = ref('');
     const score = ref(0);
     const finished = ref(false);
+    const timer = ref(0);
+    const timerInterval = ref<number | null>(null);
+    const timePerQuestion: number[] = [];
+    const timeout = ref(false);
+    const TIME_LIMIT = 25;
 
     const currentQuestion = computed(() => questions.value[currentIndex.value]);
 
+    const startTimer = () => {
+      timer.value = 0;
+      timerInterval.value = setInterval(() => {
+        timer.value++;
+        if (timer.value >= TIME_LIMIT) {
+          timeout.value = true;
+          stopTimer();
+        }
+      }, 1000);
+    }
+
+    const stopTimer = () => {
+      if (timerInterval.value !== null) {
+        clearInterval(timerInterval.value);
+        timerInterval.value = null;
+        timePerQuestion.push(Math.min(timer.value, TIME_LIMIT));
+      }
+    }
+
     const next = () => {
+      stopTimer();
       if (userAnswer.value === currentQuestion.value.answer) {
         score.value++;
       }
@@ -42,15 +71,23 @@ export default defineComponent({
       if (currentIndex.value + 1 < questions.value.length) {
         currentIndex.value++;
         userAnswer.value = '';
+        timeout.value = false;
+        startTimer();
       } else {
         finished.value = true;
       }
     }
 
+    const averageTime = computed(() => {
+      if (timePerQuestion.length === 0) return 0;
+      return timePerQuestion.reduce((a, b) => a + b, 0) / timePerQuestion.length;
+    })
+
     onMounted(async () => {
       const res = await fetch('/questions/reading.json');
       const data = await res.json();
       questions.value = data;
+      startTimer();
     })
 
     return {
@@ -60,6 +97,10 @@ export default defineComponent({
       userAnswer,
       score,
       finished,
+      timer,
+      averageTime,
+      timeout,
+      TIME_LIMIT,
       next
     }
   }
